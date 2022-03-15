28dc184072405e2d8721d103992106a9957feb04
wangkelong <1264405807@qq.com>
2022-03-15 18:49:32 +0800

    menu

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -26,13 +26,3 @@ class AcGameMenu
         
     }
 }
-class AcGame
-{
-    constructor(id)
-    {
-        this.id =id;
-        //$()表示这个是一个html元素，JQuery可通过'#'+id来找到对应id的html元素
-        this.$ac_game = $('#'+this.id);
-        this.menu = new AcGameMenu(this);
-    }
-}
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,4 +1,10 @@
-class AcGame 
+class AcGame
 {
-    constructor(id){}
+    constructor(id)
+    {
+        this.id =id;
+        //$()表示这个是一个html元素，JQuery可通过'#'+id来找到对应id的html元素
+        this.$ac_game = $('#'+this.id);
+        this.menu = new AcGameMenu(this);
+    }
 }
