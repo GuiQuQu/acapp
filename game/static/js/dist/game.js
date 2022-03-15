e985193e029f4e88dbe505269c7742fcd415a375
wangkelong <1264405807@qq.com>
2022-03-15 19:12:39 +0800

    rebot

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -8,22 +8,35 @@ class AcGameMenu
         //可以对一个html起多个class名，使用空格隔开即可
         this.$menu = $(`
             <div class="ac_game_menu">
-                <div class="ac_game_menu_field">
-                    <div class ="ac_game_menu_item ac_game_menu_single_mode">单人模式</div>
-                    <br>
-                    <div class ="ac_game_menu_item ac_game_menu_multi_mode">多人模式</div>
-                    <br>
-                    <div class ="ac_game_menu_item ac_game_menu_settings">设置</div>
-                <br>
-                </div>
+            <div class ="ac_game_menu_item ac_game_menu_single_mode">单人模式</div>
+            <div class ="ac_game_menu_item ac_game_menu_multi_mode">多人模式</div>
+            <div class ="ac_game_menu_item ac_game_menu_settings">设置</div>
             </div>
             `);
         this.root.$ac_game.append(this.$menu);
-        //find函数可以帮助我们寻找对象，使用class名前需要加上.
-        this.$single_mode = this.menu.find('.ac_game_menu_single_mode');
-        this.$multi_mode = this.menu.find(".ac_game_menu_multi_mode");
-        this.$settings = hits.menu,find(".ac_game_menu_settings");
+        this.$single_mode = this.$menu.find('ac_game_menu_single_mode');
+        this.$multi_mode = this.$menu.find("ac_game_menu_multi_mode");
+        this.$settings = this.$menu,find("ac_game_menu_settings");
         
+        this.start();
+    }
+
+    start()
+    {
+        this.add_listening_events();
+    }
+
+    add_listening_events()
+    {
+        this.$single_mode.click(function(){
+            console.log("click single_mode");
+        });
+        this.$multi_mode.click(function(){
+            console.log("click multi_mode");
+        });
+        this.$settings.click(function(){
+            console.log("click settings");
+        });
     }
 }
 class AcGame
